22b4d73563e42faf5b4808f50a58ea3e2fbd8402
Stephen Aitken <aitken.stephen@gmail.com>
2025-11-23 18:34:58 +1100

    Add /demo route embedding SYMBI Resonate demo via iframe for synchronized updates

---
+++ src/app/demo/page.tsx
@@ -0,0 +1,14 @@
+﻿'use client'
+
+export default function DemoPage() {
+  return (
+    <div style={{ height: 'calc(100vh - 80px)', width: '100%' }}>
+      <iframe
+        src="https://symbi-resonate.com/demo/"
+        style={{ border: 'none', width: '100%', height: '100%' }}
+        allow="clipboard-read; clipboard-write; fullscreen"
+        title="SYMBI Resonate Demo"
+      />
+    </div>
+  );
+}
